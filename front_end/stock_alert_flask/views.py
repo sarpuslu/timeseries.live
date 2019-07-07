c7462f30cae151a034f44bf31191ddf81371ed38
sarpuslu <sarpuslu@gmail.com>
2019-07-07 22:48:15 +0000

    added bubble plot

--- front_end/stock_alert_flask/views.py
+++ front_end/stock_alert_flask/views.py
@@ -1,13 +1,17 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 from stock_alert_flask import app
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
+import numpy as np
 import psycopg2
 import json
+import time
 import datetime
 import pytz
 import os 
+import requests
+
 
 #get credentials
 databaseName = os.environ.get("databaseName")
@@ -19,19 +23,36 @@ databasePassword = os.environ.get("databasePassword")
 db = create_engine("postgresql://{}:{}@{}:5432/{}".format(databaseUser, databaseName, databaseIP, databaseName))
 con = psycopg2.connect(host=databaseIP,database=databaseName, user=databaseUser, password=databasePassword)
 
+#@app.route('/index', methods=["POST"])
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template("index.html",
-       title = 'Home', user = { 'nickname': 'Miguel' },
-       )
+    #sinceDate = request.form.get("since_date")
+    #intensity = request.form.get("intensity")
+
+    return render_template("index.html")
 
 #list all the anomalies detected
-@app.route('/anomalies')
+@app.route('/anomalies', methods=["POST"])
 def anomalies():
     #anomalies = db.execute("select * from anomalies").fetchall()
     anomalies = db.execute("select * from stock_anomalies").fetchall()
+
+    sinceDate = request.form.get("since_date")    
+    unixSinceTime = int(time.mktime(datetime.datetime.strptime(sinceDate, "%Y-%m-%d").timetuple())*1000)
+    intensityThreshold = request.form.get("intensity")
+    
+    anomalies = db.execute("select * from stock_anomalies where time > {} and num_of_std_away > {}".format(unixSinceTime, intensityThreshold))
+    
     return render_template("anomalies.html", anomalies=anomalies)
+
+#bubble plot for all anomalies with the given condition
+@app.route('/bubbles', methods=["POST"])
+def bubbles():
+    since_date = request.form.get("since_date")
+    unixSinceTime = int(time.mktime(datetime.datetime.strptime(since_date, "%Y-%m-%d").timetuple())*1000)
+    intensityThreshold = request.form.get("intensity")
+
+    return render_template("bubbles.html", since_date=since_date, intensity=intensityThreshold)
 
 @app.route('/<ticker>')
 def ticker(ticker):
@@ -51,16 +72,8 @@ def api(ticker):
     temp =  [[data_point[0], data_point[3]] for data_point in timeseriesdata]
 
 
-    #temp2 = []
-    #for i in range(0,len(temp)-1):
-    #    if temp[i][0] != temp[i+1][0]:
-    #        temp2.append(temp[i])
+   
 
-    #for element in temp2:
-    #    element[0] = int(element[0]*1000)
-
-
-    #return json.dumps(temp2)
     return jsonify(temp)
     
 #return a list of lists of  unixtime stamps and anomaly percent changes representing anomalies
@@ -77,3 +90,41 @@ def anomalies_ticker(ticker):
 
     return jsonify(list_of_anom)
 
+
+@app.route('/bubble_json', methods=["GET"])
+def bubble_json():
+
+                               
+    since_date = request.args.get("since_date")
+    unixSinceTime = int(time.mktime(datetime.datetime.strptime(since_date, "%Y-%m-%d").timetuple())*1000)
+    intensityThreshold = request.args.get("intensity")
+
+    anomalies = db.execute("select * from stock_anomalies where time > {} and num_of_std_away > {}".format(unixSinceTime, intensityThreshold)).fetchall()
+    df = pd.DataFrame(anomalies)
+    #multiply the num_of_std_away column with the sign of percent_change
+    df[3] = df[3]*np.sign(df[2])
+    #sum to get sum of percent_change and num_of_std_away for all tickers
+    df_grouped = df.groupby([df[1]]).sum()
+    #count to get the number of anomalies since sinceDate and above intensityThreshold for all tickers
+    df_count = df.groupby([df[1]]).count()
+    #replace the sum of dates (useless) with the count of anomalies
+    df_grouped[0] = df_count[2]
+    #in the end df_grouped dataframe entries will be in the following form
+    #ZS     4  0.867157  10.175171
+
+    #create a dictionary for company information where entries are in the following form
+    #'PRNB': ['Principia Biopharma Inc.', 'Health Care', 'Major Pharmaceuticals']
+    nasdaq_companies = db.execute("select * from nasdaq_companies").fetchall()
+    company_info_dict = {}
+    for row in nasdaq_companies:
+        company_info_dict[row[0]] = [row[1], row[2], row[3]]
+
+    #put the aggregated anomaly information and company information together for the bubble plot    
+    tickers = list(df_grouped.index.values)
+    bubblePlotInput = []
+    for ticker in tickers:
+        grouped_row = list(df_grouped.loc[ticker,:])
+        insertDict = {"name":ticker, "x": grouped_row[1], "y":grouped_row[2], "z":grouped_row[0], "company_name":company_info_dict[ticker][0], "sector":company_info_dict[ticker][1], "industry":company_info_dict[ticker][2]}
+        bubblePlotInput.append(insertDict)
+    
+    return jsonify(bubblePlotInput)
